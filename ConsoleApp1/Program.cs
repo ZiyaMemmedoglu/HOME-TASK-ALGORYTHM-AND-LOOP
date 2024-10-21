d21876ead2d48d3ca1e244864897dfa9b3259a09
ZiyaMemmedoglu <ziyamm@code.edu.az>
2024-10-22 00:47:28 +0400

    home task added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,123 @@
+﻿
+
+#region
+//// homm task: 1.verilmiş n ve m (n<m) ədədləri arasındakı tək ədədlərin sayını tapın.
+
+//int n = 9; //9,11,13,15,17,19,21,23,25
+//int m = 25;
+//int count = 0;
+//if (n >= m)
+//{
+//    console.writeline(" duzgun eded daxil et");
+//}
+//else
+//{
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            count++;
+//        }
+
+//    }
+//}
+//console.writeline($"say:{count}");
+
+#endregion
+
+#region
+// home task:2 .Verilmiş n ve m (n<m) ededleri arasindaki tek ədədlərin cəmini tapin.
+
+//int n = 9; //9,11,13,15,17,19,21,23,25
+//int m = 25;
+//int sum  = 0;
+//if (n >= m)
+//{
+//    Console.WriteLine(" duzgun eded daxil et");
+//}
+//else
+//{
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            sum+=i;
+//        }
+
+//    }
+//}
+//Console.WriteLine(sum);
+#endregion
+
+#region
+//home task 3:M - ədədi verilir. 1 - dən m - ədədinə kimi olan cüt ədədlərin hasilini tapin.
+
+//int m = 11;
+//int mult = 1;
+//for (int i = 1; i <= m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        mult *= i;
+//    }
+
+//}
+//Console.WriteLine(mult);
+#endregion
+
+#region
+//home task 4:a - ədədi 3 - ə bölünən cüt ədəddirsə "3-e bolunur cutdur" ,3-ə bölünən tək ədəddirsə " ,3-e bolunur tekdir" əks halda "3-e bolunmur" sözünü ekranda çap edin.
+
+//int a = 19;
+//if (a % 3 == 0 && a % 2 == 0)
+//{
+//    Console.WriteLine(" 3 e bolunur cutdur");
+//}
+//else if (a % 3 == 0 && a % 2 == 1)
+//{
+//    Console.WriteLine(" 3 e bolunur tekdir");
+//}
+//else
+//{
+//    Console.WriteLine("3 e bolunmur");
+//}
+#endregion
+
+
+
+
+#region
+//home task:5 Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
+
+//int num = 11;
+
+//if (num == 1 && num < 0)
+
+//    Console.WriteLine(" duzgun eded daxil et");
+
+//int count = 0;
+//for (int i = 1; i <= num; i++)
+//{
+//    if (num % i == 0)
+//    {
+//        count++;
+//    }
+//}
+//if (count > 2)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else
+//{
+//    Console.WriteLine("sade");
+//}
+
+
+
+
+
+
+
+
+
+#endregion
